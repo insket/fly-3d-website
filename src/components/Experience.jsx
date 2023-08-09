e0ce6cbde66335d624540007a1fcbf04afc5028a
insket <2694183181@qq.com>
2023-08-09 16:50:11 +0800

    part2

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,6 +3,7 @@ import {
   Line,
   OrbitControls,
   PerspectiveCamera,
+  Text,
   useScroll,
 } from "@react-three/drei";
 import Background from "./Background";
@@ -13,22 +14,22 @@ import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
 
 const LINE_NB_POINTS = 12000;
-const CURVE_DISTANCE = 10;
+const CURVE_DISTANCE = 250;
+const CURVE_AHEAD_CAMERA = 0.008;
+const CURVE_AHEAD_AIRPLANE = 0.02;
+const AIRPLANE_MAX_ANGLE = 35;
 
 export const Experience = () => {
   const curvePoints = useMemo(
     () => [
       new THREE.Vector3(0, 0, 0),
-      new THREE.Vector3(0, 0, -10),
-      new THREE.Vector3(-2, 0, -20),
-      new THREE.Vector3(-3, 0, -30),
-      new THREE.Vector3(0, 0, -40),
-      new THREE.Vector3(5, 0, -50),
-      new THREE.Vector3(7, 0, -60),
-      new THREE.Vector3(5, 0, -70),
-      new THREE.Vector3(0, 0, -80),
-      new THREE.Vector3(0, 0, -90),
-      new THREE.Vector3(0, 0, -100),
+      new THREE.Vector3(0, 0, -CURVE_DISTANCE),
+      new THREE.Vector3(100, 0, -2 * CURVE_DISTANCE),
+      new THREE.Vector3(-100, 0, -3 * CURVE_DISTANCE),
+      new THREE.Vector3(100, 0, -4 * CURVE_DISTANCE),
+      new THREE.Vector3(0, 0, -5 * CURVE_DISTANCE),
+      new THREE.Vector3(0, 0, -6 * CURVE_DISTANCE),
+      new THREE.Vector3(0, 0, -7 * CURVE_DISTANCE),
     ],
     []
   );
@@ -54,37 +55,44 @@ export const Experience = () => {
   const scroll = useScroll();
 
   useFrame((state, delta) => {
-    const curPointIndex = Math.min(
-      Math.round(scroll.offset * linePoints.length),
-      linePoints.length - 1
-    );
-    const curPoint = linePoints[curPointIndex];
-    const pointAhead =
-      linePoints[Math.min(curPointIndex + 1, linePoints.length - 1)];
+    const scrollOffset = Math.max(0, scroll.offset);
 
-    const xDisplacement = (pointAhead.x - curPoint.x) * 80;
-    const angleRotation =
-      (xDisplacement < 0 ? 1 : -1) *
-      Math.min(Math.abs(xDisplacement), Math.PI / 3);
+    const curPoint = curve.getPointAt(scrollOffset);
 
-    const targetAirplaneQuaternion = new THREE.Quaternion().setFromEuler(
-      new THREE.Euler(
-        airplane.current.rotation.x,
-        airplane.current.rotation.y,
-        angleRotation
-      )
-    );
-    const targetCameraQuaternion = new THREE.Quaternion().setFromEuler(
-      new THREE.Euler(
-        cameraGroup.current.rotation.x,
-        angleRotation,
-        cameraGroup.current.rotation.z
-      )
-    );
-
-    airplane.current.quaternion.slerp(targetAirplaneQuaternion, delta * 2);
-    cameraGroup.current.quaternion.slerp(targetCameraQuaternion, delta * 2);
     cameraGroup.current.position.lerp(curPoint, delta * 24);
+
+    const lookAtPoint = curve.getPointAt(
+      Math.min(scrollOffset + CURVE_DISTANCE, 1)
+    );
+
+    const currenrLookAt = cameraGroup.current.getWorldDirection(
+      new THREE.Vector3()
+    );
+    const targetLookAt = new THREE.Vector3()
+      .subVectors(curPoint, lookAtPoint)
+      .normalize();
+    const lookAt = currenrLookAt.lerp(targetLookAt, delta * 24);
+
+    cameraGroup.current.lookAt(
+      cameraGroup.current.position.clone().add(lookAt)
+    );
+
+    const tangent = curve.getTangent(scrollOffset + CURVE_AHEAD_AIRPLANE);
+
+    const nonLerpLookAt = new THREE.Group();
+    nonLerpLookAt.position.copy(curPoint);
+    nonLerpLookAt.lookAt(nonLerpLookAt.position.clone().add(targetLookAt));
+
+    tangent.applyAxisAngle(
+      new THREE.Vector3(0, 1, 0),
+      -nonLerpLookAt.rotation.y
+    );
+
+    let angle = Math.atan2(-tangent.z, tangent.x);
+    angle = -Math.PI / 2 + angle;
+
+    let angleDegrees = (angle * 180) / Math.PI;
+    angleDegrees *= 2.4; // stronger angle
   });
 
   return (
@@ -95,7 +103,7 @@ export const Experience = () => {
         <Background />
         <PerspectiveCamera position={[0, 0, 5]} fov={30} makeDefault />
         <group ref={airplane}>
-          <Float floatIntensity={2} speed={2}>
+          <Float floatIntensity={1} speed={1.5} rotationIntensity={0.5}>
             <Airplane
               rotation-y={Math.PI / 2}
               scale={[0.2, 0.2, 0.2]}
@@ -103,6 +111,34 @@ export const Experience = () => {
             />
           </Float>
         </group>
+      </group>
+
+      {/* Text */}
+      <group position={[-3, 0, -100]}>
+        <Text
+          color={"white"}
+          anchorX={"left"}
+          anchorY={"middle"}
+          fontSize={0.22}
+          maxWidth={2.5}
+          font={"./fonts/DMSerifDisplay-Regular.ttf"}
+        >
+          Welcome to my portfolio! {"\n"}
+          I'm a fullstack developer, and I love to create
+        </Text>
+      </group>
+
+      <group position={[-10, 1, -200]}>
+        <Text
+          color={"white"}
+          anchorX={"left"}
+          anchorY={"top"}
+          fontSize={0.32}
+          maxWidth={2.5}
+          font={"./fonts/DMSerifDisplay-Regular.ttf"}
+        >
+          Do you want to know more about me?
+        </Text>
       </group>
 
       {/* Line */}
@@ -122,7 +158,23 @@ export const Experience = () => {
         </mesh>
       </group>
 
-      <Cloud opacity={0.5} scale={0.3} position={[-2, 1, -3]} />
+      {/* CLOUDS */}
+      <Cloud scale={[1, 1, 1.5]} position={[-3.5, -1.2, -7]} />
+      <Cloud scale={[1, 1, 2]} position={[3.5, -1, -10]} rotation-y={Math.PI} />
+      <Cloud
+        scale={[1, 1, 1]}
+        position={[-3.5, 0.2, -12]}
+        rotation-y={Math.PI / 3}
+      />
+      <Cloud scale={[1, 1, 1]} position={[3.5, 0.2, -12]} />
+
+      <Cloud
+        scale={[0.4, 0.4, 0.4]}
+        rotation-y={Math.PI / 9}
+        position={[1, -0.2, -12]}
+      />
+      <Cloud scale={[0.3, 0.5, 2]} position={[-4, -0.5, -53]} />
+      <Cloud scale={[0.8, 0.8, 0.8]} position={[-1, -1.5, -100]} />
     </>
   );
 };
